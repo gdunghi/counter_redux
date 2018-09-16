f025516a573dbb25d5e2e2bd7c38f63edc35d38e
jirayu <jirayu.photisan@gmail.com>
2018-09-16 13:37:01 +0700

    first commit

---
+++ lib/appState.dart
@@ -0,0 +1,6 @@
+class AppState {
+  final int count;
+  AppState({this.count});
+
+  factory AppState.initial() => new AppState(count: 69); //init
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:counter_redux/appState.dart';
+import 'package:counter_redux/reducers/appReducers.dart';
+import 'package:counter_redux/actions/appActions.dart';
+
+// void main() => runApp(new MyApp());
+void main() {
+  final store =
+      new Store<AppState>(appReducer, initialState: AppState.initial());
+  runApp(new MyApp(
+    store: store,
+    title: 'Flutter Redux Demo',
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final Store<AppState> store;
+  final String title;
+
+  MyApp({Key key, this.store, this.title}) : super(key: key);
+
+  // final String title;
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider<AppState>(
+        store: store,
+        child: new MaterialApp(
+          title: 'Flutter Demo',
+          theme: new ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: new MyHomePage(title: 'Flutter Demo Home Page'),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new StoreConnector<AppState, int>(
+              converter: (Store<AppState> store) => store.state.count,
+              builder: (context, count) {
+                return new Text(
+                  count.toString(),
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new StoreConnector<AppState, VoidCallback>(
+        converter: (store) {
+          return () => store.dispatch(IncrementalAction());
+        },
+        builder: (context, callback) {
+          return new FloatingActionButton(
+            onPressed: callback,
+            tooltip: 'Increment',
+            child: new Icon(Icons.add),
+          );
+        },
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
